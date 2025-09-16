33f6c979a61fd513a8efc96429cd73ef22598556
jardani <vitorboarreto@outlook.com>
2025-09-16 09:33:38 -0300

    :books: EstudosJava

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,11 @@
+/** 
+    Hello World
+    @author Vitor Boarreto
+    Meu primeiro Hello World em Java!
+*/
+
+public class Hello{/*estrutura precisa ter o mesmo nome do arquivo */
+    public static void main(String[] args){ /**ponto de inicio */
+        System.out.println("Hello World");
+    }
+}
